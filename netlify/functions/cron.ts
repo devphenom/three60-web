bf30a5426788cd0e18aa3c0c036a53f174292595
devphenom <iphenom01@gmail.com>
2023-09-09 05:22:00 +0100

    feat- implement cron job for overdue todos

--- netlify/functions/cron.ts
+++ netlify/functions/cron.ts
@@ -1,14 +1,27 @@
-import { schedule } from '@netlify/functions';
+import { schedule, HandlerEvent, HandlerContext } from '@netlify/functions';
+import clientPromise from '../../utils/mongo-db';
+import Todos from '../../features/todos/services/todos-schema';
 
-const cronHandler = async (event: any, context: any) => {
-  // Do something scheduled
-  console.log('Scheduled function called');
-  return {
-    statusCode: 200,
-    body: 'OK',
-  };
+const cronHandler = async (event: HandlerEvent, context: HandlerContext) => {
+  try {
+    await clientPromise();
+    const updatedTodos = await Todos.updateMany(
+      {
+        statusId: { $in: [1, 2] },
+        // todoEndTime: { $lt: new Date() }, // TODO: uncomment this when todo end time is initialized.
+      },
+      { $set: { statusId: 4 } },
+    );
+
+    return {
+      statusCode: 200,
+      body: 'OK',
+    };
+  } catch (error) {
+    return { statusCode: 500, error: 'An error occurred' };
+  }
 };
 
-const handler = schedule('* * * * *', cronHandler);
+const handler = schedule('@hourly', cronHandler);
 
 export { handler };
